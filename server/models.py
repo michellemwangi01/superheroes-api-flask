49c08c386bbf00e48c9a6c2de9b3cf6f001dddb3
michellemwangi01 <michellemwangim@gmail.com>
2023-10-02 12:18:24 +0300

    removed bcryptt

--- server/models.py
+++ server/models.py
@@ -2,7 +2,6 @@ from sqlalchemy.ext.associationproxy import association_proxy
 from sqlalchemy.orm import validates
 from sqlalchemy import MetaData
 from flask_sqlalchemy import SQLAlchemy
-import bcrypt
 
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
@@ -48,26 +47,6 @@ class Power(db.Model):
             raise ValueError("Description must be longer than 20 chars")
         else:
             return description
-
-
-class User(db.Model):
-    __tablename__ = 'users'
-
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String)
-    password_hash = db.Column(db.String)
-
-    def __repr__(self):
-        return f'(id={self.id}, username={self.username})'
-
-    def set_password(self, password):
-        # Generate a password hash
-        password_hash = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
-        self.password_hash = password_hash.decode('utf-8')
-
-    def check_password(self, password):
-        # Check if a provided password matches the stored hash
-        return bcrypt.checkpw(password.encode('utf-8'), self.password_hash.encode('utf-8'))
 
 
 
